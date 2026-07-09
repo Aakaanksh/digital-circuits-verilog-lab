3819a3dd5ed579e898e98f933ca0a4bb21a167bb
D YS Aakaanksh <ysaakaankshdevathi1524@gmail.com>
2026-07-09 22:38:57 +0530

    Refactor half adder module with new syntax

--- Lab-01-Structural-Modeling/Lab-01-Structural-Modeling/rtl/half_adder.v
+++ Lab-01-Structural-Modeling/Lab-01-Structural-Modeling/rtl/half_adder.v
@@ -1,11 +1,7 @@
-module half_adder (
-    input  a,
-    input  b,
-    output sum,
-    output carry
-);
-
-assign sum   = a ^ b;
-assign carry = a & b;
-
+`timescale 1ns / 1ps
+module ha(a,b,s,cy);
+input a,b;
+output s,cy;
+xor x1 (s,a,b);
+and a1(cy,a,b);
 endmodule
